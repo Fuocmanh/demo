e79fecbe592d860e80863c94e3694c7d5aaa9794
FuocManh <manh7nui@gmail.com>
2022-12-30 16:59:56 +0700

    upload source code

---
+++ js/style.js
@@ -0,0 +1,10 @@
+var nav = document.getElementById('nav');
+function hideShowNav(){
+    display=nav.style.display ;
+    display=='none'?nav.style.display ='block':nav.style.display ='none';
+    // nav.style.opacity='1';
+}
+// function onchane(){
+//     let width = document.getElementsByClassName('.box').style.width='50%';
+// console.log(width);
+// }
